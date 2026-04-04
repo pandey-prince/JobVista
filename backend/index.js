fe86b2c5f55ead3d855cd9dcb7561e56d9b8b5a0
Prince Pandey <work.princepandey@gmail.com>
2026-04-04 23:52:47 +0530

    updated cors setting

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
