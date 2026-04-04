3f6dd09ebaee45ecf8cf7435ec9e027290683b33
Prince Pandey <work.princepandey@gmail.com>
2026-04-04 23:57:30 +0530

    updated cors setting

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://job-vista-eta.vercel.app",
+    credentials: true,
+  }),
+);
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
