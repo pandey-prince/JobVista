9cb0451f10e19712a6afaf2fdba766ce0242f763
Prince Pandey <work.princepandey@gmail.com>
2026-04-04 23:49:59 +0530

    updated constant.js

--- frontend-codes/frontend/src/utils/constant.js
+++ frontend-codes/frontend/src/utils/constant.js
@@ -1,4 +1,12 @@
-export const USER_API_END_POINT = process.env.USER_API_END_POINT;
-export const JOB_API_END_POINT = process.env.JOB_API_END_POINT;
-export const APPLICATION_API_END_POINT = process.env.APPLICATION_API_END_POINT;
-export const COMPANY_API_END_POINT = process.env.COMPANY_API_END_POINT;
+export const USER_API_END_POINT =
+  //   process.env.USER_API_END_POINT |
+  "https://jobvista-ahek.onrender.com/api/v1/user";
+export const JOB_API_END_POINT =
+  //   process.env.JOB_API_END_POINT |
+  "https://jobvista-ahek.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT =
+  //   process.env.APPLICATION_API_END_POINT |
+  "https://jobvista-ahek.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  //   process.env.COMPANY_API_END_POINT |
+  "https://jobvista-ahek.onrender.com/api/v1/company";
