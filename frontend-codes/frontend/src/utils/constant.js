f2168e2e6c559b2019e558ddb137ef0cd39d2870
Prince Pandey <work.princepandey@gmail.com>
2026-04-05 02:37:01 +0530

    updated constant.js

--- frontend-codes/frontend/src/utils/constant.js
+++ frontend-codes/frontend/src/utils/constant.js
@@ -1,13 +1,13 @@
 export const USER_API_END_POINT = import.meta.env.VITE_USER_API_END_POINT;
-// "https://jobvista-ahek.onrender.com/api/v1/user";
+
 //"http://localhost:8000/api/v1/user";
 export const JOB_API_END_POINT = import.meta.env.VITE_JOB_API_END_POINT;
-//"https://jobvista-ahek.onrender.com/api/v1/job";
+
 //"http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT = 
-import.meta.env.VITE_APPLICATION_API_END_POINT;
-// "https://jobvista-ahek.onrender.com/api/v1/application";
+export const APPLICATION_API_END_POINT = import.meta.env
+  .VITE_APPLICATION_API_END_POINT;
+
 //"http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = import.meta.env.COMPANY_API_END_POINT;
-// "https://jobvista-ahek.onrender.com/api/v1/company";
+export const COMPANY_API_END_POINT = import.meta.env.VITE_COMPANY_API_END_POINT;
+
 //"http://localhost:8000/api/v1/company";
