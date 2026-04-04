9b87dd9c6d78921ae26d6eb48867de815e30215c
Prince Pandey <work.princepandey@gmail.com>
2026-04-05 00:16:48 +0530

    updated cookies

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = await req.cookies.token;
+    console.log(req.cookies);
     console.log(token);
     if (!token) {
       return res.status(401).json({
