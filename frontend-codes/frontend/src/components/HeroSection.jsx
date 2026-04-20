6f0e11cb2af6faf7b074765eed8ba44f4674b762
Prince Pandey <work.princepandey@gmail.com>
2026-04-21 00:33:47 +0530

    Revise description in HeroSection component
    Updated the description in HeroSection component to provide more relevant information about the job platform.

--- frontend-codes/frontend/src/components/HeroSection.jsx
+++ frontend-codes/frontend/src/components/HeroSection.jsx
@@ -20,7 +20,9 @@ const HeroSection = () => {
             <div className='flex flex-col gap-5 my-10'>
                 <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur temporibus nihil tempora dolor!</p>
+                <p>
+                    Find the right opportunities faster with our AI-powered job platform. Build your profile, showcase your skills, and get matched with jobs that truly fit your experience and career goals.
+                </p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
