cbfe631a37c42fb70f9773ec37596c9168742723
Prince Pandey <work.princepandey@gmail.com>
2026-07-21 21:38:51 +0530

    Default the site theme to light mode for first-time visitors.

--- frontend-codes/frontend/src/components/shared/ThemeProvider.jsx
+++ frontend-codes/frontend/src/components/shared/ThemeProvider.jsx
@@ -1,7 +1,7 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const ThemeProvider = ({ children, ...props }) => (
-  <NextThemesProvider attribute="class" defaultTheme="system" enableSystem storageKey="jobvista-theme" {...props}>
+  <NextThemesProvider attribute="class" defaultTheme="light" enableSystem storageKey="jobvista-theme" {...props}>
     {children}
   </NextThemesProvider>
 );
